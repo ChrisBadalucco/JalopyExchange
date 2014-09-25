a11c75449e1792c3a35ca080a8eb9d25b507121b
Chris Badalucco <badaluccoc843@strose.edu>
2014-09-24 22:11:18 -0400

    added some constraints

--- grails-app/domain/com/je/domain/User.groovy
+++ grails-app/domain/com/je/domain/User.groovy
@@ -4,8 +4,15 @@ class User {
 
     String firstName
     String lastName
-    boolean isSeller
+    String email
+    String username
+    String password
+    Integer age
 
     static constraints = {
+        username size: 5..15, blank: false, unique: true
+        password size: 5..15, blank: false
+        email email: true, blank: false
+        age min: 18
     }
 }
